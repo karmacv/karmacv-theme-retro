9401bcfc90794006288f7292dd70222662de9da3
Elton Marku <eltonmarku@gmail.com>
2020-12-05 21:25:39 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var Handlebars = require('handlebars');
+const { registerHelper } = require('handlebars');
+const { compileSassAndSaveMultiple } = require('compile-sass');
 
 Handlebars.registerHelper('debugContext', function (optionalValue) {
   console.log('Current Context');
@@ -14,6 +16,13 @@ Handlebars.registerHelper('debugContext', function (optionalValue) {
   }
 });
 
+Handlebars.registerHelper( 'isEmpty', function( data, opts ) {
+  if (Array.isArray(data) && data.length > 0 || Object.keys(data).length > 0) {
+      return data;
+  }
+  return opts.fn(data);;
+});
+
 Handlebars.registerHelper( 'debugVariable', function( data, breakpoint ) {
     console.log(data);
     if (breakpoint === true) {
@@ -22,12 +31,28 @@ Handlebars.registerHelper( 'debugVariable', function( data, breakpoint ) {
     return '';
 });
 
+Handlebars.registerHelper('ifIn', function(elem, list, options) {
+  if(list.indexOf(elem) > -1) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 function render(resume) {
   var css = fs.readFileSync(__dirname + '/style.css', 'utf-8');
   var tpl = fs.readFileSync(__dirname + '/resume.hbs', 'utf-8');
   var partialsDir = path.join(__dirname, 'partials');
-  var filenames = fs.readdirSync(partialsDir);
+  var partialsOther = path.join(__dirname, 'partials-other');
+  registerPartial(partialsDir);
+  registerPartial(partialsOther);
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume,
+  });
+}
 
+function registerPartial(partialsDir) {
+  const filenames = fs.readdirSync(partialsDir);
   filenames.forEach(function (filename) {
     var matches = /^([^.]+).hbs$/.exec(filename);
     if (!matches) {
@@ -39,12 +64,8 @@ function render(resume) {
 
     Handlebars.registerPartial(name, template);
   });
-  return Handlebars.compile(tpl)({
-    css: css,
-    resume: resume,
-  });
 }
 
 module.exports = {
-  render: render,
+  render: render
 };
